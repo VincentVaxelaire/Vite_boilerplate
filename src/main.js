0e332ec00caa188b9de7f5053d8ba9046feaf966
Vincent Vaxelaire <68623931+VincentVaxelaire@users.noreply.github.com>
2023-04-11 22:58:30 +0200

    add simple spa
    it's bad code, have to find a way generate view link on build time

--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
 import './style.css'
 
-console.log('Hello World')
+import { router, navigateTo } from './js/router.js'
+import { canvas } from './js/canvas.js'
+
+canvas(document.querySelector('#webgl'));
+
+window.addEventListener('popstate', () => {
+    router()
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const navLinks = document.querySelectorAll('[data-navigation]');
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault()
+
+            navigateTo(e.target.href)
+        })
+    })
+
+    router()
+})
